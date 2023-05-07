63ae9a9e12abac41cab4e8d4d11817927aefb93c
FarLemon <FarLemon.FFC@gmail.com>
2023-05-07 08:36:08 -0400

    Add 'Food and Drink' item group

---
+++ src/main/java/net/far/harvest_homestead/item/ModItemGroup.java
@@ -0,0 +1,19 @@
+package net.far.harvest_homestead.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.far.harvest_homestead.HarvestHomestead;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+    public static ItemGroup FOOD_AND_DRINK;
+
+    public static void registerItemGroups() {
+        FOOD_AND_DRINK = FabricItemGroup.builder(new Identifier(HarvestHomestead.MOD_ID, "food&drink"))
+            .displayName(Text.translatable("itemgroup.food&drink"))
+            .icon(() -> new ItemStack(ModItems.TOMATO))
+            .build();
+    }
+}
